0336ae0e1e1cb3d31c867747912a833c79d0d54e
junesdreyfus <junes.dreyfus@gmail.com>
2025-07-30 18:51:28 -0400

    Menu model angles
    *got the coordonates down for each rotation of the 3D model so far.
    *some clean ups
    *some messes

--- js/3Dmodel.js
+++ js/3Dmodel.js
@@ -6,13 +6,17 @@ const threedeeDiv = document.getElementById('threedee');
 
 const renderer = new THREE.WebGLRenderer();
 
+//3D window size (scales the model!!!)
+
 renderer.setSize(1920, 1920);
 threedeeDiv.appendChild(renderer.domElement);
+
+//ambient light
 
 const ambientLight = new THREE.AmbientLight('rgba(250, 237, 193, 1)', 1); 
 scene.add(ambientLight);
 
-// Flag to control rotation
+// boolean to control rotation
 let rotation1 = false; 
 let rotation2 = false;
 let rotation3 = false; 
@@ -26,11 +30,26 @@ let model;
 loader.load('3Dmodel/skingrafting.glb', function (gltf) {
     model = gltf.scene;
     scene.add(model);
+ 
+//initial angle of station
+        let station={
+        posx : 2,
+        posy : -2,
+        posz : -12.2,
+
+        angx : 0.2,
+        angy : 0,
+        angz : 0,
+
+        scax : 0.1,
+        scay : 0.1,
+        scaz : 0.1,
+        }  ;
 
     //coordonates of model, its scale, and camera position/
-    model.position.set(2, -2, -12.2);
-    model.rotation.set(0.2, 0, 0);
-    model.scale.set(0.1, 0.1, 0.1);
+    model.position.set(station.posx, station.posy, station.posz);
+    model.rotation.set(station.angx, station.angy, station.angz);
+    model.scale.set(station.scax, station.scay, station.scaz);
     
     camera.position.set(1, -4, 1);
 
@@ -99,23 +118,43 @@ function animate() {
 //     // console.log(camera.position)
 
 //     // collision();
+
+let animationspeed = 0.05;
+
  if (rotation1) {
     //model.rotation.axis += (goalangle - model.rotation.axis) * speed
-     model.rotation.y += (2 - model.rotation.y) * 0.03;
+     model.rotation.y += (5 - model.rotation.y) * animationspeed;
+     model.position.x += (-7 - model.position.x) * animationspeed
+     model.position.y += (-5.5 - model.position.y) * animationspeed
+     model.position.z += (10 - model.position.z) * animationspeed
  }
  if (rotation2) {
     //model.rotation.axis += (goalangle - model.rotation.axis) * speed
-     model.rotation.x += (4 - model.rotation.x) * 0.03;
+     model.rotation.x += (3.5 - model.rotation.x) * animationspeed;
+     model.position.x += (-15 - model.position.x)*animationspeed;
+     model.position.y += (5 - model.position.y)*animationspeed;
+     model.position.z += (-30 - model.position.z)*animationspeed;
  }
    if (rotation3) {
-    model.rotation.y += (-3 -  model.rotation.y) * 0.05;
+    model.rotation.y += (-5 -  model.rotation.y) * animationspeed;
+    model.position.z += (7.5 - model.position.z) * animationspeed
+     model.position.y += (-5 - model.position.y) * animationspeed
  }
    if (rotation4) {
-    model.rotation.y -= 0.01;
+     model.rotation.y += (1.5 - model.rotation.y) * animationspeed;
+     model.position.x += (1 - model.position.x) * animationspeed
+     model.position.y += (-3.2 - model.position.y) * animationspeed
+     model.position.z += (-4.8 - model.position.z) * animationspeed
+
  } else {
-    model.rotation.x += (0.2 -  model.rotation.x) * 0.05;
-    model.rotation.y += (0 -  model.rotation.y) * 0.05;
-    model.rotation.z += (0 -  model.rotation.z) * 0.05;
+    //should make a function for this. this resets the changes into the original value.
+    model.rotation.x += (0.2-  model.rotation.x) * animationspeed;
+    model.rotation.y += (0 -  model.rotation.y) * animationspeed;
+    model.rotation.z += (0 -  model.rotation.z) * animationspeed;
+
+    model.position.x += (2 - model.position.x) * animationspeed
+    model.position.y += (-2 - model.position.y) * animationspeed
+    model.position.z += (-12.2 - model.position.z) * animationspeed
  } 
     renderer.render(scene, camera);
 
@@ -123,7 +162,7 @@ function animate() {
 }
 ["mouseover", "mouseout"].forEach(event => {
      
-  
+  //triggers rotation each time mouse hover in or out.
     document.getElementById("button1").addEventListener(event, () => {
         rotation1 = !rotation1;
 
