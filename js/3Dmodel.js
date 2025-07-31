ac62a88d6d2a40c53a8d520b22324878d81be857
gamerjeannedielman <junes.dreyfus@gmail.com>
2025-07-31 11:56:46 -0400

    model fixes
    *3D model fixes
    *added tools to change lighting eventually
    *clean up

--- js/3Dmodel.js
+++ js/3Dmodel.js
@@ -1,18 +1,40 @@
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, 750 / 500, 0.1, 1000);
 
 const threedeeDiv = document.getElementById('threedee');
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias : true});
+
+//3D window size (scales the model!!!)
 
 renderer.setSize(1920, 1920);
 threedeeDiv.appendChild(renderer.domElement);
 
-const ambientLight = new THREE.AmbientLight('rgba(250, 237, 193, 1)', 1); 
+//initial angle of station
+        let station={
+        posx : 2,
+        posy : -2,
+        posz : -12.2,
+
+        angx : 0.2,
+        angy : 0,
+        angz : 0,
+
+        scax : 0.1,
+        scay : 0.1,
+        scaz : 0.1,
+
+        light1 : 1.3,
+        }  ;
+
+//ambient light
+
+const ambientLight = new THREE.AmbientLight('rgba(255, 255, 255, 1)', station.light1); 
 scene.add(ambientLight);
 
-// Flag to control rotation
+
+
+// boolean to control rotation
 let rotation1 = false; 
 let rotation2 = false;
 let rotation3 = false; 
@@ -22,15 +44,19 @@ const loader = new THREE.GLTFLoader();
 
 let model;
 
+
+
 //loading the 3D model/
 loader.load('3Dmodel/skingrafting.glb', function (gltf) {
     model = gltf.scene;
     scene.add(model);
+ 
+
 
     //coordonates of model, its scale, and camera position/
-    model.position.set(2, -2, -12.2);
-    model.rotation.set(0.2, 0, 0);
-    model.scale.set(0.1, 0.1, 0.1);
+    model.position.set(station.posx, station.posy, station.posz);
+    model.rotation.set(station.angx, station.angy, station.angz);
+    model.scale.set(station.scax, station.scay, station.scaz);
     
     camera.position.set(1, -4, 1);
 
@@ -81,49 +107,60 @@ function getForwardVector() {
 function animate() {
     	  requestAnimationFrame( animate );
 
-          
-        // const forward = getForwardVector(); // Get the forward direction relative to the camera
-        // const right = new THREE.Vector3().crossVectors(forward, new THREE.Vector3(0, 1, 0)); // Right direction (perpendicular to forward)
+let animationspeed = 0.025;
 
-//         // Set velocity based on where the camera's looking rather than just general static directions
-//         if (moveForward) velocity.add(forward.multiplyScalar(speed));  // Move forward
-//         if (moveBackward) velocity.add(forward.multiplyScalar(-speed));  // Move backward
-//         if (moveLeft) velocity.add(right.multiplyScalar(-speed));  // Move left
-//         if (moveRight) velocity.add(right.multiplyScalar(speed));  // Move right
-    
+function resetmodel(){
+        //should make a function for this. this resets the changes into the original value.
+    model.rotation.x += (station.angx-  model.rotation.x) * animationspeed;
+    model.rotation.y += (station.angy -  model.rotation.y) * animationspeed;
+    model.rotation.z += (station.angz -  model.rotation.z) * animationspeed;
 
-//     // Update the camera position
-//     camera.position.add(velocity);
-//     velocity.set(0, 0, 0);
-// // I can use this when I'll do the collisions/
-//     // console.log(camera.position)
+    model.position.x += (station.posx - model.position.x) * animationspeed
+    model.position.y += (station.posy - model.position.y) * animationspeed
+    model.position.z += (station.posz - model.position.z) * animationspeed
 
-//     // collision();
+}
+
+
  if (rotation1) {
     //model.rotation.axis += (goalangle - model.rotation.axis) * speed
-     model.rotation.y += (2 - model.rotation.y) * 0.03;
+     model.rotation.y += (5 - model.rotation.y) * animationspeed;
+     model.position.x += (-7 - model.position.x) * animationspeed
+     model.position.y += (-5.5 - model.position.y) * animationspeed
+     model.position.z += (10 - model.position.z) * animationspeed
+
+    // // ambientLight.intensity -= 0.005; // Dim the light
+    // ambientLight.intensity -= (1 - ambientLight.intensity) * 0.009; // Dim the light
+
  }
  if (rotation2) {
     //model.rotation.axis += (goalangle - model.rotation.axis) * speed
-     model.rotation.x += (4 - model.rotation.x) * 0.03;
+     model.rotation.x += (2.9 - model.rotation.x) * animationspeed;
+     model.position.x += (-15 - model.position.x)* animationspeed;
+     model.position.y += (9 - model.position.y)* animationspeed;
+     model.position.z += (-30 - model.position.z)*animationspeed;
  }
    if (rotation3) {
-    model.rotation.y += (-3 -  model.rotation.y) * 0.05;
+    model.rotation.y += (-5 -  model.rotation.y) * animationspeed;
+    model.position.z += (7.5 - model.position.z) * animationspeed
+     model.position.y += (-5 - model.position.y) * animationspeed
  }
    if (rotation4) {
-    model.rotation.y -= 0.01;
+     model.rotation.y += (1.5 - model.rotation.y) * animationspeed;
+     model.position.x += (1 - model.position.x) * animationspeed
+     model.position.y += (-3.2 - model.position.y) * animationspeed
+     model.position.z += (-4.8 - model.position.z) * animationspeed
+
  } else {
-    model.rotation.x += (0.2 -  model.rotation.x) * 0.05;
-    model.rotation.y += (0 -  model.rotation.y) * 0.05;
-    model.rotation.z += (0 -  model.rotation.z) * 0.05;
+resetmodel();
  } 
     renderer.render(scene, camera);
-
-
 }
+
+
 ["mouseover", "mouseout"].forEach(event => {
      
-  
+  //triggers rotation each time mouse hover in or out.
     document.getElementById("button1").addEventListener(event, () => {
         rotation1 = !rotation1;
 
@@ -144,5 +181,3 @@ function animate() {
 //   y: THREE.MathUtils.radToDeg(model.rotation.y),
 //   z: THREE.MathUtils.radToDeg(model.rotation.z)
 // });
-
-
