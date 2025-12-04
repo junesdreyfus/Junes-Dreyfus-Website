32b5d46209afef093b63ae321bab63b1bbd9787c
gamerjeannedielman <junes.dreyfus@gmail.com>
2025-12-04 15:26:48 +0100

    oh god
    - ../

--- js/journal.js
+++ js/journal.js
@@ -10,9 +10,9 @@ let journalcontent =`
               ">
       <p style="width:80%; margin-left: 10%">4th of december 2025 : I feel chilly.<br>
       <video controls width="90%"><source src="../assets/journal/4.mp4" type="video/mp4"></video>
-      <img src="../assets/journal/1.webp" style="width:90%";>
-      <img src="../assets/journal/2.webp" style="width:90%";>
-      <img src="../assets/journal/3.webp" style="width:90%";>
+      <img src="assets/journal/1.webp" style="width:90%";>
+      <img src="assets/journal/2.webp" style="width:90%";>
+      <img src="assets/journal/3.webp" style="width:90%";>
       </p>
     </div>`
 
