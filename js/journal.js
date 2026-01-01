df17e8ee4ef0f82ad7d91e04e3b8c2ab3f2f6331
gamerjeannedielman <junes.dreyfus@gmail.com>
2026-01-01 18:38:00 +0100

    The Great Styles Merger
    *merged my two unnecessary styles.css, kept the old one in case.
    *Fixed Journal size on mobile
    *fixed note gas station

--- js/journal.js
+++ js/journal.js
@@ -8,12 +8,12 @@ let journalcontent =`
               position: fixed;"
               onclick="togglejournal()";> X </button
               ">
-      <p style="width:80%; margin-left: 10%">New! <a href="/gestures/most-loved-of2025.html"><b><i>Most Loved Of 2025</b></i></a><br>
+      <p style="width:80%; margin-left: 10%"><a href="/gestures/most-loved-of2025.html"><b><i><u>New Page! Most Loved Of 2025</u></b></i></a><br>
       02/12/2025 : I feel chilly.
       <video controls width="90%"><source src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/4.mp4" type="video/mp4"></video>
-      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/1.webp" style="width:90%";>
-      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/2.webp" style="width:90%";>
-      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/3.webp" style="width:90%";>
+      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/1.webp" ;>
+      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/2.webp" ;>
+      <img src="https://junesdreyfus.github.io/Junes-Dreyfus-Website/assets/journal/3.webp" ;>
       </p>
     </div>`
 
