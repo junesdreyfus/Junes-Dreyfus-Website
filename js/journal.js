8c4cba052d08aaa3bf4c0549dcd8b9654935717f
gamerjeannedielman <junes.dreyfus@gmail.com>
2026-01-10 17:21:34 +0100

    pre-release overhaul
    *Added some doodles that work on both mobile and desktop
    *Fixed some of the text that was lame
    *Added and replaced some images.
    *Added journal entry

--- js/journal.js
+++ js/journal.js
@@ -5,8 +5,12 @@ let journalcontent =`
     <button onclick="togglejournal()";> X </button
               ">
       <p style="width:80%; margin-left: 10%"><a href="../gestures/most-loved-of2025.html"><b><i><u>New Page! Most Loved Of 2025</u></b></i></a><br>
-      7/01/2026:
       <video controls width="90%"><source src="../assets/journal/4.mp4" type="video/mp4"></video>
+      10/01/2026: god damn this thing is finally live! welcome everyone, thank you to everyone who lent a hand. This is still very much unpolished, especially on mobile, but I'm quite happy with the result ╏] .
+       We're still at the stage where it's not considered gauche to report on any bug so feel free to do so at junes.dreyfus@gmail.com
+       (say anything after that and i kill you).
+       <a href="https://youtube.com/playlist?list=OLAK5uy_n8Oj3-IQitRGpSMr2ex1rC6-VopMzdDv8&si=z97Wf7L9n1YfQJ_Y" target="_blank">ANyway you can listen to this album i like very much.</a>
+
       <img src="../assets/journal/1.webp" ;>
       <img src="../assets/journal/2.webp" ;>
       <img src="../assets/journal/3.webp" ;>
